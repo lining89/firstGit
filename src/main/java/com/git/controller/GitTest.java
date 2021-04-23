acec702e1a4ef0d6f98192164d9b4211059d3965
admin <lining_1129@163.com>
2021-04-23 14:46:21 +0800

    提交一个测试类

---
+++ src/main/java/com/git/controller/GitTest.java
@@ -0,0 +1,12 @@
+package com.git.controller;
+
+/**
+ * @author Mr ● Li
+ * @version 1.0
+ * @date 2021/4/23 14:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
