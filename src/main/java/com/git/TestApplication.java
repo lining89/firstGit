075bdaf70c32f9cc9753afc4054ca0e566850cd9
admin <lining_1129@163.com>
2021-05-12 14:24:16 +0800

    上传文件和添加nacos配置

--- src/main/java/com/git/TestApplication.java
+++ src/main/java/com/git/TestApplication.java
@@ -3,9 +3,11 @@ package com.git;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.git")
 @MapperScan("com.git.mapper")
+@EnableDiscoveryClient
 public class TestApplication {
 
     public static void main(String[] args) {
